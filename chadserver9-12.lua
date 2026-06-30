85c46e8839764795a6f97f382a8b313d2c4a88c4
Messer252 <90804794+Messer252@users.noreply.github.com>
2026-06-30 00:01:19 -0500

    Create chadserver9-12.lua

---
+++ chadserver9-12.lua
@@ -0,0 +1,126 @@
+-- ================================
+-- Chad.Net SERVER v9 (STABLE)
+-- ================================
+
+local PROTOCOL = "chadnet"
+
+-------------------------------------------------
+-- MODEM BOOT
+-------------------------------------------------
+local modem = peripheral.find("modem")
+if not modem then error("No modem found") end
+rednet.open(peripheral.getName(modem))
+
+print("Chad.Net Server V9 Online")
+
+-------------------------------------------------
+-- STATE
+-------------------------------------------------
+local clients = {} -- id -> {name,lastSeen}
+
+-------------------------------------------------
+local function send(id,msg)
+    rednet.send(id,msg,PROTOCOL)
+end
+
+local function broadcast(msg)
+    rednet.broadcast(msg,PROTOCOL)
+end
+
+-------------------------------------------------
+local function getUsers()
+    local list = {}
+    for _,v in pairs(clients) do
+        table.insert(list,v.name)
+    end
+    return list
+end
+
+-------------------------------------------------
+local function getCount()
+    local c = 0
+    for _ in pairs(clients) do c = c + 1 end
+    return c
+end
+
+-------------------------------------------------
+while true do
+    local id,msg = rednet.receive(PROTOCOL)
+    if type(msg) ~= "table" then goto continue end
+
+    -------------------------------------------------
+    -- LOGIN
+    -------------------------------------------------
+    if msg.type == "login" then
+        clients[id] = {name=msg.user,lastSeen=os.clock()}
+
+        send(id,{type="login_ok"})
+        broadcast({type="system",text=msg.user.." joined"})
+    end
+
+    -------------------------------------------------
+    -- PING
+    -------------------------------------------------
+    if msg.type == "ping" and clients[id] then
+        clients[id].lastSeen = os.clock()
+        send(id,{type="pong"})
+    end
+
+    -------------------------------------------------
+    -- CHAT
+    -------------------------------------------------
+    if msg.type == "chat" and clients[id] then
+        broadcast({
+            type="chat",
+            text="["..clients[id].name.."]: "..msg.text
+        })
+    end
+
+    -------------------------------------------------
+    -- USERS + COUNT FIX
+    -------------------------------------------------
+    if msg.type == "user_list" then
+        send(id,{
+            type="user_list",
+            users=getUsers(),
+            count=getCount()
+        })
+    end
+
+    -------------------------------------------------
+    -- TTT INVITE (FIXED ROUTING)
+    -------------------------------------------------
+    if msg.type == "ttt_challenge" then
+        local from = clients[id] and clients[id].name
+        if not from then goto continue end
+
+        local targetId
+        for cid,v in pairs(clients) do
+            if v.name == msg.target then
+                targetId = cid
+                break
+            end
+        end
+
+        if targetId then
+            send(targetId,{
+                type="ttt_invite",
+                from=from
+            })
+        else
+            send(id,{type="system",text="User not found"})
+        end
+    end
+
+    -------------------------------------------------
+    -- CLEANUP
+    -------------------------------------------------
+    for cid,v in pairs(clients) do
+        if os.clock() - v.lastSeen > 12 then
+            broadcast({type="system",text=v.name.." disconnected"})
+            clients[cid]=nil
+        end
+    end
+
+    ::continue::
+end
